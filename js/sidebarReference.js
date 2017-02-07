ca1c0cacd17a01cdd6ca19e606d7750964ac4e7c
Jake Larson <jacob.larson@colorado.edu>
2017-02-07 16:06:30 -0700

    Create sidebarReference.js
    This file is generated based on available datasets.  Must be updated when new data is added to server.

---
+++ js/sidebarReference.js
@@ -0,0 +1,85 @@
+var sidebar = {
+  "RCP": {
+    "rcp85": {
+      "antDyn": {
+        "IPCC AR5": {
+          "low": {
+            "ref": "adl1"
+          },
+          "high": {
+            "ref": "adh1"
+          },
+          "medium": {
+            "ref": "adm1"
+          }
+        }
+      },
+      "greenDyn": {
+        "IPCC AR5": {
+          "low": {
+            "ref": "gdl1"
+          },
+          "high": {
+            "ref": "gdh1"
+          },
+          "medium": {
+            "ref": "gdm1"
+          }
+        }
+      },
+      "greenSmb": {
+        "NaN": {},
+        "IPCC AR5": {
+          "low": {
+            "ref": "gsl1"
+          },
+          "high": {
+            "ref": "gsh1"
+          },
+          "medium": {
+            "ref": "gsm1"
+          }
+        }
+      },
+      "thermo": {
+        "IPCC AR5": {
+          "low": {
+            "ref": "thl1"
+          },
+          "high": {
+            "ref": "thh1"
+          },
+          "medium": {
+            "ref": "thm1"
+          }
+        }
+      },
+      "antSmb": {
+        "IPCC AR5": {
+          "low": {
+            "ref": "asl1"
+          },
+          "high": {
+            "ref": "ash1"
+          },
+          "medium": {
+            "ref": "asm1"
+          }
+        }
+      },
+      "glacier": {
+        "IPCC AR5": {
+          "low": {
+            "ref": "gll1"
+          },
+          "high": {
+            "ref": "glh1"
+          },
+          "medium": {
+            "ref": "glm1"
+          }
+        }
+      }
+    }
+  }
+};
